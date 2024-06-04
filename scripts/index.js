100911f77bd166c2c56c6b1f87f04559d14ae6e9
Vadim Ter-Simonyan <vadim.ter-simonyan@yandex.ru>
2024-06-05 00:09:47 +0300

    bug fixes

--- scripts/index.js
+++ scripts/index.js
@@ -3,23 +3,23 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-const createCard = (name, link) => {
+const createCard = (el, deleteCard) => {
   const element = cardTemplate.cloneNode(true);
   const img = element.querySelector(".card__image");
   const title = element.querySelector(".card__title");
   const deleteButton = element.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", (evt) => deleteCard(evt));
-  img.src = link;
-  title.textContent = name;
-  img.alt = name;
+  img.src = el.link;
+  title.textContent = el.name;
+  img.alt = el.name;
   return element;
-}
+};
 // @todo: Функция удаления карточки
 const deleteCard = (evt) => {
   evt.target.closest(".card").remove();
 };
 // @todo: Вывести карточки на страницу
 initialCards.forEach(el => {
-  const card = createCard(el.name, el.link);
+  const card = createCard(el, deleteCard);
   placesList.append(card);
 });
